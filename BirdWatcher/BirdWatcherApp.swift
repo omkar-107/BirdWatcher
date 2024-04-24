933e82ddb84cbc37d59be9dacbf8bd502e3e2447
omkar <salunkheomkar107@gmail.com>
2024-04-24 14:11:49 +0530

    Initial Commit

---
+++ BirdWatcher/BirdWatcherApp.swift
@@ -0,0 +1,17 @@
+//
+//  BirdWatcherApp.swift
+//  BirdWatcher
+//
+//  Created by mini project on 24/04/24.
+//
+
+import SwiftUI
+
+@main
+struct BirdWatcherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BirdWatcher/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BirdWatcher
+//
+//  Created by mini project on 24/04/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
